f16f28eefd90c5d11d70688a03a93c2b75c46c7a
DESKTOP-036U8E0\beaut <mj.shin@twolinecode.com>
2021-01-25 10:48:55 +0900

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//수동 component 추가
+import { ComponentOverviewComponent } from './cExample1.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentOverviewComponent
   ],
   imports: [
     BrowserModule,
