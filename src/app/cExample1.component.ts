5360082845c9ff2f0311d54bb8398c6322ead0bb
DESKTOP-036U8E0\beaut <mj.shin@twolinecode.com>
2021-01-25 10:19:10 +0900

    angular project1

---
+++ src/app/cExample1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-component-overview',
+    template:  `<h1> Hello World! </h1>
+                <p> This Template definition spans multiple lines. </p>`
+})
+
+export class ComponentOverviewComponent{
+
+}
